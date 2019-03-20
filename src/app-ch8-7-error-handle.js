fe1ef0ae7b39f56f506a9b9c1d174a85ed9c1415
morrxy <morrxy@gmail.com>
2019-03-20 13:30:49 +0800

    add a either example

---
+++ src/app-ch8-7-error-handle.js
@@ -0,0 +1,58 @@
+// import * as S from 'sanctuary';
+import { log } from './utils/helper';
+
+class Either {
+  static of(x) {
+    return new Right(x);
+  }
+
+  constructor(x) {
+    this.$value = x;
+  }
+}
+
+class Left extends Either {
+  map() {
+    return this;
+  }
+
+  inspect() {
+    return `Left(${this.$value})`;
+  }
+}
+
+class Right extends Either {
+  map(f) {
+    return Either.of(f(this.$value));
+  }
+
+  inspect() {
+    return `Right(${this.$value})`;
+  }
+}
+
+const left = x => new Left(x);
+
+log(
+  Either.of('rain')
+    .map(str => `b${str}`)
+    .inspect()
+);
+
+log(
+  left('rain')
+    .map(str => `It's gonna ${str}, better bring your umbrella!`)
+    .inspect()
+);
+
+log(
+  Either.of({ host: 'localhost', port: 80 })
+    .map(x => x.host)
+    .inspect()
+);
+
+log(
+  left('rolls eyes...')
+    .map(x => x.host)
+    .inspect()
+);
