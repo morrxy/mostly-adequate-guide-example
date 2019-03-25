47932496848ca09d75819770bf96c7f5344ef92b
morrxy <morrxy@gmail.com>
2019-03-25 10:50:06 +0800

    add a node example

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ runable example for [Professor Frisby’s Mostly Adequate Guide to Functional Pr
 1. `npm start`
 2. open `http://localhost:8080/webpack-dev-server` in browser
 3. click those html file, and see the console.
+
+## how to run node example
+
+`node path-to-example-file`
+
+`node -r esm src-node/app-ch8-13-async.js`
---
+++ src-node/app-ch8-13-async.js
@@ -0,0 +1,18 @@
+import { Task } from '../src/utils/Task';
+import { split, head } from '../src/utils/point-free';
+
+const fs = require('fs');
+const path = require('path');
+
+// readFile :: String -> Task Error String
+const readFile = filename =>
+  new Task((reject, result) => {
+    fs.readFile(filename, 'utf8', (err, data) => (err ? reject(err) : result(data)));
+  });
+
+const filePath = path.join(__dirname, 'metamorphosis');
+const firstLine = readFile(filePath)
+  .map(split('\n'))
+  .map(head);
+
+firstLine.fork(err => console.log(String(err)), str => console.log(str));
