bae26895259fb7eabb520d40cd7121d5e602704a
morrxy <morrxy@gmail.com>
2019-04-22 22:08:24 +0800

    add a ch9 exercise

---
+++ src/app-ch9-13-exercise.js
@@ -0,0 +1,17 @@
+// import { chain, map } from './utils/point-free';
+// import { compose } from './utils/essential-fn';
+// import { IO } from './utils/IO';
+
+// consider helpers with the following signatures:
+
+// validateEmail :: Email -> Either String Email
+// addToMailingList :: Email -> IO([Email])
+// emailBlast :: [Email] -> IO ()
+
+// Use `validateEmail`, `addToMailingList` and `emailBlast` to create a function which adds a new email to the mailing list if valid, and then notify the whole list.
+
+// joinMailingList :: Email -> Either String (IO ())
+// const joinMailingList = compose(
+//   map(compose(chain(emailBlast), addToMailingList)),
+//   validateEmail
+// );
